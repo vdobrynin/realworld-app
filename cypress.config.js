3b9ee25b0a1b8518954b01bb4d3a33a1ac14e112
Vasiliy Dobrynin <vdobrynin@sbcglobal.net>
2024-04-10 09:30:55 -0500

    env: changes at command.js & cypress.config

--- cypress.config.js
+++ cypress.config.js
@@ -5,13 +5,18 @@ module.exports = defineConfig({
   viewportWidth: 2560,
   video: true,             // 'false' for do not run
   // videoCompression: 32,    // slower
+  env: {
+    username: 'pwtest60@test.com',
+    password: 'vd12345',
+    apiUrl: 'https://conduit-api.bondaracademy.com'
+  }
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
     baseUrl: "https://conduit.bondaracademy.com/",
-    specPattern: "cypress/integration/**/*.spec.{js,jsx,ts,tsx}", //,
-    // excludeSpecPattern: [
-    //   "**/1-getting-started/*", "**/2-advanced-examples/*"
+    specPattern: "cypress/integration/**/*.spec.{js,jsx,ts,tsx}",
+    excludeSpecPattern: [
+      "**/1-getting-started/*", "**/2-advanced-examples/*", '**/examples/*'
   }
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -40,11 +40,11 @@ Cypress.Commands.add('loginToApplication', () => {
     //                                               // headless authorization #42
     const userCredentials = {
         "user": {
-            "email": "pwtest60@test.com",
-            "password": "vd12345"
+            "email": Cypress.env("username"),
+            "password": Cypress.env("password")
         }
     }
-    cy.request('POST', 'https://conduit-api.bondaracademy.com/api/users/login', userCredentials)
+    cy.request('POST', Cypress.env('apiUrl+/api/users/login'), userCredentials)
         .its('body').then(body => {
             const token = body.user.token
             cy.wrap(token).as('token')
