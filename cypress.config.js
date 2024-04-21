d301653f6cf919bd8784e997213755f001b0d1b0
Vasiliy Dobrynin <vdobrynin@sbcglobal.net>
2024-04-20 23:36:20 -0700

    retries in cypress.config file globally or particular test  adds in 2nd test

--- cypress.config.js
+++ cypress.config.js
@@ -4,20 +4,29 @@ module.exports = defineConfig({
   viewportHeight: 1440,
   viewportWidth: 2560,
   video: false,
+  reporter: 'cypress-multi-reporters',
+  reporterOptions: {
+    configFile: 'reporter-config.json'
+  },
   env: {
     username: "pwtest60@test.com",
     password: "",
     apiUrl: "https://conduit-api.bondaracademy.com",
   },
+  retries: {
+    runMode: 2,
+    openMode: 0
+  },
+
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
       const username = process.env.DB_USERNAME
       const password = process.env.PASSWORD
 
-      if (!password) {
-        throw new Error(`missing PASSWORD environment variable`)
-      }
+      // if (!password) {
+      //   throw new Error(`missing PASSWORD environment variable`)
+      // }
 
       config.env = { username, password }
       return config
--- cypress/integration/secondTest.spec.js
+++ cypress/integration/secondTest.spec.js
@@ -5,9 +5,9 @@ describe('Test log out', () => {
         cy.loginToApplication()
     })
 
-    it('verify use can log out successfully', () => {
+    it('verify use can log out successfully', { retries: 2 }, () => {
         cy.contains('Settings').click()
         cy.contains('Or click here to logout').click()
-        cy.get('.navbar-nav').should('contain', 'Sign up')
+        cy.get('.navbar-nav').should('contain', 'Sign up1')
     })
 })
