e37cec4bef34514f4556f04d5e78c1f068ce85ce
Vasiliy Dobrynin <vdobrynin@sbcglobal.net>
2024-04-21 23:47:21 -0700

    delete type error from tests previously

--- cypress/integration/secondTest.spec.js
+++ cypress/integration/secondTest.spec.js
@@ -8,6 +8,6 @@ describe('Test log out', () => {
     it('verify use can log out successfully', { retries: 2 }, () => {
         cy.contains('Settings').click()
         cy.contains('Or click here to logout').click()
-        cy.get('.navbar-nav').should('contain', 'Sign up1')
+        cy.get('.navbar-nav').should('contain', 'Sign up')
     })
 })
