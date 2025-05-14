2ca229497a30255dafeee3328e2bb9ccae3d99d6
Vasiliy Dobrynin <vdobrynin@sbcglobal.net>
2025-05-13 19:49:52 -0700

    review & fix scripts for Multiple Test Reports #47

--- cypress/integration/secondTest.spec.js
+++ cypress/integration/secondTest.spec.js
@@ -6,10 +6,10 @@ describe('Test log out', () => {
     })
 
     it('verify use can log out successfully', { retries: 2 }, () => {  // #46
-    // it('verify use can log out successfully', () => {
+        // it('verify use can log out successfully', () => {
         cy.contains('Settings').click()
         cy.contains('Or click here to logout').click()
-        cy.get('.navbar-nav').should('contain', 'Sign up') 
-        // cy.get('.navbar-nav').should('contain', 'Sign up1') // #46 for check how many retries
+        cy.get('.navbar-nav').should('contain', 'Sign up')
+        // cy.get('.navbar-nav').should('contain', 'Sign up2') // #46 for check how many retries
     })
 })
